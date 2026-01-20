486b69257a1a9ab7245a81c03792210396f852ec
pudhuraajesh <pudhuraajesh2006@gmail.com>
2026-01-20 14:02:36 +0530

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -9,14 +9,15 @@ def detect_word_pattern(pattern, text):
         print("No word patterns detected.")
 
 sample_inputs = [
-    (r"\bcat\b", "Hey great Gamers gather here,right?"),
-    (r"\b\d{4}\b", "I was born in the year of 2005"),
-    (r"\b[a-zA-Z]{5}\b", "Hello,I am Nish,And this is python"),
-    (r"\bing\b", "Gaming is a great stress buster")
+    (r"\bfor\b", "Hello for Everyone"),
+    (r"\b\d{4}\b", "I was born in the year of 2006"),
+    (r"\b[a-zA-Z]{5}\b", "Hello,I am Raajesh"),
+    (r"\bing\b", "Drinking Water is good for our body")
 ]
 
 for pattern, text in sample_inputs:
     print("Pattern:", pattern)
     print("Text:", text)
     detect_word_pattern(pattern, text)
+
     print("-" * 40)
